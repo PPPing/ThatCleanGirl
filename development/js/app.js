d898dd1a19a305ce285b597be0158aae768e89b6
ping.develop <zhongyp.develop@gmail.com>
2015-01-15 00:37:27 +0800

    [Demo] update Top Menu and Sider with angularJS

--- development/js/app.js
+++ development/js/app.js
@@ -1,5 +1,115 @@
 (function(){
 	var app = angular.module('thatCleanGirl', [ ]);
-
-		
-	})();
+	
+	var components = [
+		{
+			id:"topMenu1",
+			name:"Client Management",
+			modules:[
+				{
+					id:"clientList",
+					name:"Client List",
+					url:"",
+					active:true
+				},
+				{
+					id:"newClient",
+					name:"New Client",
+					active:false
+				}
+			]
+		},
+		{
+			id:"topMenu2",
+			name:"Staff Management",
+			modules:[
+				{
+					id:"staffList",
+					name:"Staff List",
+					url:"",
+					active:true
+				}
+			]
+		}
+	];
+	var modules = [
+		{
+			id:"clientList",
+			name:"Client List",
+			url:"",
+			active:true
+		},
+		{
+			id:"newClient",
+			name:"New Client",
+			active:false
+		}
+	];
+	app.service('MenuService', function () {
+		this.components=[
+			{
+				id:"topMenu1",
+				name:"Client Management",
+				modules:[
+					{
+						id:"clientList",
+						name:"Client List",
+						url:"",
+						active:true
+					},
+					{
+						id:"newClient",
+						name:"New Client",
+						active:false
+					}
+				]
+			},
+			{
+				id:"topMenu2",
+				name:"Staff Management",
+				modules:[
+					{
+						id:"staffList",
+						name:"Staff List",
+						url:"",
+						active:true
+					}
+				]
+			}
+		];
+		this.module = [
+			{
+				id:"clientList",
+				name:"Client List",
+				url:"",
+				active:true
+			},
+			{
+				id:"newClient",
+				name:"New Client",
+				active:false
+			}		
+		];
+	});
+	app.controller('TopMenuController',['MenuService',function(MenuService){
+		$scope.test = "test";
+		this.components = components;
+		this.curComponentIndex = 0;
+		this.switchComponent = function(index){
+			alert("Switch Component. \nIndex : " +  index + " - "+components[index].name);
+			this.curComponentIndex = index;
+			modules = components[index].modules;
+		};
+	}]);
+	
+	app.controller('SiderController',['MenuService',function(MenuService){
+		this.modules = modules;
+		this.curModuleIndex = 0;
+		this.switchModule = function(index){
+			alert("Switch Module. \nIndex : " +  index +"\n"+$scope.test);
+			this.modules = modules;
+			this.curModuleIndex = index;
+		};
+	}]);
+	
+})();
