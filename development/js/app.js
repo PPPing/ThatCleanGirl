3a0c2e97ebd2c01dd06a9795befc99fcb3cd6f7d
ping.develop <zhongyp.develop@gmail.com>
2015-01-16 00:59:32 +0800

    [Demo]create MenuSerive in app.js

--- development/js/app.js
+++ development/js/app.js
@@ -1,115 +1,113 @@
 (function(){
 	var app = angular.module('thatCleanGirl', [ ]);
 	
-	var components = [
-		{
-			id:"topMenu1",
-			name:"Client Management",
-			modules:[
+	app.controller('MainController',['$scope',function($scope){
+		$scope.components=[
 				{
-					id:"clientList",
-					name:"Client List",
-					url:"",
-					active:true
+					id:"topMenu1",
+					name:"Client Management",
+					modules:[
+						{
+							id:"clientList",
+							name:"Client List",
+							url:"",
+							active:true
+						},
+						{
+							id:"newClient",
+							name:"New Client",
+							active:false
+						}
+					]
 				},
 				{
-					id:"newClient",
-					name:"New Client",
-					active:false
+					id:"topMenu2",
+					name:"Staff Management",
+					modules:[
+						{
+							id:"staffList",
+							name:"Staff List",
+							url:"",
+							active:true
+						}
+					]
 				}
-			]
-		},
-		{
-			id:"topMenu2",
-			name:"Staff Management",
-			modules:[
-				{
-					id:"staffList",
-					name:"Staff List",
-					url:"",
-					active:true
-				}
-			]
-		}
-	];
-	var modules = [
-		{
-			id:"clientList",
-			name:"Client List",
-			url:"",
-			active:true
-		},
-		{
-			id:"newClient",
-			name:"New Client",
-			active:false
-		}
-	];
-	app.service('MenuService', function () {
-		this.components=[
-			{
-				id:"topMenu1",
-				name:"Client Management",
-				modules:[
-					{
-						id:"clientList",
-						name:"Client List",
-						url:"",
-						active:true
-					},
-					{
-						id:"newClient",
-						name:"New Client",
-						active:false
-					}
-				]
-			},
-			{
-				id:"topMenu2",
-				name:"Staff Management",
-				modules:[
-					{
-						id:"staffList",
-						name:"Staff List",
-						url:"",
-						active:true
-					}
-				]
-			}
-		];
-		this.module = [
-			{
-				id:"clientList",
-				name:"Client List",
-				url:"",
-				active:true
-			},
-			{
-				id:"newClient",
-				name:"New Client",
-				active:false
-			}		
-		];
-	});
-	app.controller('TopMenuController',['MenuService',function(MenuService){
-		$scope.test = "test";
-		this.components = components;
-		this.curComponentIndex = 0;
-		this.switchComponent = function(index){
-			alert("Switch Component. \nIndex : " +  index + " - "+components[index].name);
-			this.curComponentIndex = index;
-			modules = components[index].modules;
-		};
+			];
+		$scope.curComponentIndex=1;
+		$scope.modules=$scope.components[$scope.curComponentIndex].modules;;
+		$scope.$on('someEvent', function(event, data){
+			console.log('someEvent');
+		});
 	}]);
-	
-	app.controller('SiderController',['MenuService',function(MenuService){
-		this.modules = modules;
-		this.curModuleIndex = 0;
-		this.switchModule = function(index){
-			alert("Switch Module. \nIndex : " +  index +"\n"+$scope.test);
-			this.modules = modules;
-			this.curModuleIndex = index;
+	app.factory('MenuService',function(){
+		var components=[
+				{
+					id:"topMenu1",
+					name:"Client Management",
+					modules:[
+						{
+							id:"clientList",
+							name:"Client List",
+							url:"",
+							active:true
+						},
+						{
+							id:"newClient",
+							name:"New Client",
+							active:false
+						}
+					]
+				},
+				{
+					id:"topMenu2",
+					name:"Staff Management",
+					modules:[
+						{
+							id:"staffList",
+							name:"Staff List",
+							url:"",
+							active:true
+						}
+					]
+				}
+			];
+		var curComponentIndex = 0;
+		var MenuService = {
+			getComponents:function(){
+				return components;
+			},
+			getModules:function(){
+				return components[curComponentIndex].modules;
+			},
+			changeComponents:function(id){
+				curComponentIndex = id;
+			}
+		}
+		return MenuService;
+	});
+	app.controller('TopMenuController',['$scope','MenuService',function($scope,MenuService){
+		this.components = MenuService.getComponents();
+		$scope.changeComponents = MenuService.changeComponents;
+		console.log(this.components);
+		/* $scope.curComponentIndex=0;
+		this.switchComponent = function(index){
+			$scope.modules = this.components[index].modules;
+			$scope.curComponentIndex = index;
+			MenuService.updateId();
+			console
 		};
+		
+		this.switchComponent(0); */
+	}]);	
+	app.controller('SiderController',['$scope','MenuService',function($scope,MenuService){
+		$scope.getModules = MenuService.getModules;
+		//$scope.updateId = MenuService.updateId;
+		//this.switchModule = function(index){
+			//console.log($scope.curComponentIndex);
+			/* alert("Switch Module. \nIndex : " +  index +"\n");
+			this.modules = modules;
+			this.curModuleIndex = index; */
+		//};
 	}]);
 	
 })();
