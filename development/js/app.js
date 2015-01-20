07e69b3a11f3328149e96c2ac28289cfb8f7a11c
ping.develop <zhongyp.develop@gmail.com>
2015-01-21 00:23:55 +0800

    [Demo]  Impletement Component and Module change

--- development/js/app.js
+++ development/js/app.js
@@ -1,44 +1,6 @@
 (function(){
 	var app = angular.module('thatCleanGirl', [ ]);
-	
-	app.controller('MainController',['$scope',function($scope){
-		$scope.components=[
-				{
-					id:"topMenu1",
-					name:"Client Management",
-					modules:[
-						{
-							id:"clientList",
-							name:"Client List",
-							url:"",
-							active:true
-						},
-						{
-							id:"newClient",
-							name:"New Client",
-							active:false
-						}
-					]
-				},
-				{
-					id:"topMenu2",
-					name:"Staff Management",
-					modules:[
-						{
-							id:"staffList",
-							name:"Staff List",
-							url:"",
-							active:true
-						}
-					]
-				}
-			];
-		$scope.curComponentIndex=1;
-		$scope.modules=$scope.components[$scope.curComponentIndex].modules;;
-		$scope.$on('someEvent', function(event, data){
-			console.log('someEvent');
-		});
-	}]);
+
 	app.factory('MenuService',function(){
 		var components=[
 				{
@@ -46,13 +8,13 @@
 					name:"Client Management",
 					modules:[
 						{
-							id:"clientList",
+							id:"client-list",
 							name:"Client List",
 							url:"",
 							active:true
 						},
 						{
-							id:"newClient",
+							id:"new-client",
 							name:"New Client",
 							active:false
 						}
@@ -63,7 +25,7 @@
 					name:"Staff Management",
 					modules:[
 						{
-							id:"staffList",
+							id:"staff-list",
 							name:"Staff List",
 							url:"",
 							active:true
@@ -72,6 +34,7 @@
 				}
 			];
 		var curComponentIndex = 0;
+		var curModuleIndex = 0;
 		var MenuService = {
 			getComponents:function(){
 				return components;
@@ -79,8 +42,16 @@
 			getModules:function(){
 				return components[curComponentIndex].modules;
 			},
-			changeComponents:function(id){
-				curComponentIndex = id;
+			getActiveModule:function(){
+				//console.log(components[curComponentIndex].modules[curModuleIndex]);
+				return components[curComponentIndex].modules[curModuleIndex];
+			},
+			changeComponents:function(index){
+				curComponentIndex = index;
+			},
+			changeModule:function(index){
+				curModuleIndex = index;
+				console.log(components[curComponentIndex].modules[curModuleIndex]);
 			}
 		}
 		return MenuService;
@@ -89,25 +60,53 @@
 		this.components = MenuService.getComponents();
 		$scope.changeComponents = MenuService.changeComponents;
 		console.log(this.components);
-		/* $scope.curComponentIndex=0;
-		this.switchComponent = function(index){
-			$scope.modules = this.components[index].modules;
-			$scope.curComponentIndex = index;
-			MenuService.updateId();
-			console
-		};
-		
-		this.switchComponent(0); */
+
 	}]);	
 	app.controller('SiderController',['$scope','MenuService',function($scope,MenuService){
 		$scope.getModules = MenuService.getModules;
-		//$scope.updateId = MenuService.updateId;
-		//this.switchModule = function(index){
-			//console.log($scope.curComponentIndex);
-			/* alert("Switch Module. \nIndex : " +  index +"\n");
-			this.modules = modules;
-			this.curModuleIndex = index; */
-		//};
+		$scope.changeModule = MenuService.changeModule;
 	}]);
 	
+	app.directive('moduleContainer',function($compile) {
+		return {
+			restrict: 'E',
+			template: '<div></div>',
+			scope: {},
+			controller:function($scope,$element,MenuService) {
+				$scope.getActiveModule = MenuService.getActiveModule;
+				$scope.$watch(
+                    function( $scope ) {
+                        console.log( "Function watched" );
+                        // This becomes the value we're "watching".
+                        return $scope.getActiveModule().id;
+                    },
+                    function( newValue ) {
+                        var el = $compile( "<module-"+newValue+"></module-"+newValue+">" )( $scope );
+						$element.html( el );
+                    }
+                );				
+			}
+		};
+	});
+	app.directive('moduleClientList',function() {
+		return {
+			restrict: 'E',
+			template: '<div>module Client List</div>',
+			scope: {}
+		};
+	});
+	app.directive('moduleNewClient',function() {
+		return {
+			restrict: 'E',
+			template: '<div>module New Client</div>',
+			scope: {}
+		};
+	});
+	app.directive('moduleStaffList',function() {
+		return {
+			restrict: 'E',
+			template: '<div>module Staff List</div>',
+			scope: {}
+		};
+	});
 })();
